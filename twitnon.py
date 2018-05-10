df246cd412b5129a204d5e7d0abc3ed3ec998f82
HushBugger <hushbugger@airmail.cc>
2018-05-10 15:31:49 +0200

    Support tweets with multiple images

--- twitnon.py
+++ twitnon.py
@@ -11,30 +11,32 @@ with open(os.path.join(os.path.dirname(__file__), 'accounts.txt')) as f:
 now = datetime.datetime.now()
 cutoff = now - datetime.timedelta(days=7)
 
-found = {}
+covered = set()
+imgs = []
 
 for acc in accs:
     soup = bs4.BeautifulSoup(requests.get(acc).text, 'html.parser')
     for tweet in soup.find_all('li', 'stream-item'):
-        photo = tweet.find('div', 'js-adaptive-photo')
-        if photo is None:
-            continue
-        img_url = photo.find('img')['src'] + ':orig'
         name = tweet.find(class_='fullname').text
         perma = tweet.find('a', 'js-permalink')
         permalink = f"https://twitter.com{perma['href']}"
         conv_id = perma['data-conversation-id']
         time = datetime.datetime.fromtimestamp(
             int(perma.find('span')['data-time']))
-        if time < cutoff:
+        if time < cutoff or conv_id in covered:
             continue
-        found[conv_id] = (time, f'''<div>
+        for i, photo in enumerate(tweet.find_all('div', 'js-adaptive-photo')):
+            img_url = photo.find('img')['src'] + ':orig'
+            # tweak the time so images appear in order
+            imgs.append((time - datetime.timedelta(microseconds=i),
+                         f'''<div>
 <strong><a href="{permalink}">{name}</a></strong><br />
 {time}<br />
 <a href="{img_url}"><img src="{img_url}" /></a><br />
 <a href="{img_url}" class="source">{img_url}</a><br />
 <a href="{permalink}" class="source">{permalink}</a>
-</div>''')
+</div>'''))
+        covered.add(conv_id)
 
 print(f'''<!DOCTYPE html><html><head>
 <title>Twitnon report {now}</title>
@@ -44,8 +46,8 @@ img { width: 200px; max-width: 100%; }
 a.source { font-size: 0.4em; }
 </style>
 </head><body>''')
-print(''.join(
-    tw[1] for tw in sorted(found.values(), key=lambda x: x[0], reverse=True)))
+for img in sorted(imgs, key=lambda x: x[0], reverse=True):
+    print(img[1])
 print('''<br /><br />
 <a href="https://github.com/hushbugger/twitnon">Source code</a>
 </body></html>''')
