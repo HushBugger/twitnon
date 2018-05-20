71842fa1d619a928eefd2bef04c752c1720df63b
HushBugger <hushbugger@airmail.cc>
2018-05-20 13:41:01 +0200

    Add automatic redirect

--- twitnon.sh
+++ twitnon.sh
@@ -1,4 +1,18 @@
 #!/bin/sh
+fname=$(date -I).html
 mkdir -p out
-./twitnon.py out/$(date -I).html
-date -I
+./twitnon.py out/$fname
+cat > out/index.html <<EOF
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8"/>
+    <script type="text/javascript">
+      window.location.replace("/$fname");
+    </script>
+  </head>
+  <body>
+    <p>Redirecting to <a href="/$fname">$fname</a></p>
+  </body>
+</html>
+EOF
